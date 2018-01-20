7ea1282556fc8a0247bf53bf01652aa699157383
Xinyi Chen <xic98@pitt.edu>
2018-01-20 00:47:10 -0500

    Single Page Applications Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { DISABLED } from '@angular/forms/src/model';
 
 @Injectable()
 export class DishService {
@@ -9,5 +10,13 @@ export class DishService {
 
   getDishes(): Dish[] {
     return DISHES;
+  }
+
+  getDish(id: number): Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => (dish.featured))[0];
   }
 }
